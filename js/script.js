69f671f419660d604f8f498d5c7e3f18fab95f85
Eray <erayozhan.contact@gmail.com>
2023-11-23 23:22:23 +0300

    Film Detayları Sayfası
    Signed-off-by: Eray <erayozhan.contact@gmail.com>

--- js/script.js
+++ js/script.js
@@ -86,6 +86,76 @@ async function displayPopularShows(){
   })
 }
 
+// Film detayları Sayfası
+async function displayMovieDetails(){
+
+  // Soru işaretinden sonraki kısmı gösterir 
+  const movieId = location.search.split("=")[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`)
+
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+      <div class="details-top">
+          <div>
+          ${
+            movie.poster_path ? 
+              `
+                <img
+                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                class="card-img-top"
+                alt="${movie.title}"
+                />
+              `
+              :
+              `
+                <img
+                src="../images/no-image.jpg"
+                class="card-img-top"
+                alt="${movie.title}"
+                />
+             `
+
+      }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(2)} / 10
+            </p>
+            <p class="text-muted">Çıkış Tarihi: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Kategoriler</h5>
+            <ul class="list-group">
+              ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Film Sayfasını Ziyaret Et</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Film Hakkında</h2>
+          <ul>
+            <li><span class="text-secondary">Bütçe:</span> $${addCommasToMoney(movie.budget)}</li>
+            <li><span class="text-secondary">Gelir:</span> $${addCommasToMoney(movie.revenue)}</li>
+            <li><span class="text-secondary">Film Süresi:</span> ${movie.runtime} dakika</li>
+            <li><span class="text-secondary">Durum:</span> ${
+              movie.status === "Released" ?  
+                "Yayınlandı" :
+                "Yayınlanmadı"
+            }</li>
+          </ul>
+          <h4>Yapımcılar</h4>
+          ${movie.production_companies.map(company => `<span>${company.name}</span>`).join(', ')}
+        </div>
+  `
+
+  document.getElementById("movie-details").appendChild(div);
+}
+
 
 // TMDB api'den data fetchleme
 async function fetchAPIData(endpoint){
@@ -123,6 +193,11 @@ function markLink() {
   });
 }
 
+// Para değerine ',' ekleme
+function addCommasToMoney(number){
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
 // Sayfaları yönlendirme (router)
 function init() {
   if (global.currentPage === "/" || global.currentPage === "/index.html") {
@@ -130,7 +205,7 @@ function init() {
   } else if (global.currentPage === "/shows.html") {
     displayPopularShows();
   } else if (global.currentPage === "/movie-details.html") {
-    console.log("Film Hakkında");
+    displayMovieDetails();
   } else if (global.currentPage === "/tv-details.html") {
     console.log("Dizi Hakkında");
   } else if (global.currentPage === "/search.html") {
