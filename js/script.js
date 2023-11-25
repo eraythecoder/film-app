a2db1c6fa1fb3ccc72625e181f9ec42af5d34f4a
Eray <erayozhan.contact@gmail.com>
2023-11-25 22:18:08 +0300

    Anasayfaya Slider Eklendi
    Signed-off-by: Eray <erayozhan.contact@gmail.com>

--- js/script.js
+++ js/script.js
@@ -254,6 +254,52 @@ function displayBackgroundImage(type, backgroundPath){
   }
 }
 
+// Slider 
+async function displaySlider(){
+  const { results } = await fetchAPIData("movie/now_playing")
+
+  results.forEach(movie =>{
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide")
+
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="${movie.title}" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(2)} / 10
+      </h4>
+    `
+    document.querySelector(".swiper-wrapper").appendChild(div);
+
+    initSwiper()
+  })
+}
+
+function initSwiper(){
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4500,
+      disableOnIntereaction: false
+    },
+    breakpoints:{
+      500:{
+        slidesPerView: 2
+      },
+      700:{
+        slidesPerView: 3
+      },
+      1200:{
+        slidesPerView: 4
+      }
+    } 
+  })
+}
+
 // TMDB api'den data fetchleme
 async function fetchAPIData(endpoint){
   const API_KEY = "";
@@ -298,6 +344,7 @@ function addCommasToMoney(number){
 // Sayfaları yönlendirme (router)
 function init() {
   if (global.currentPage === "/" || global.currentPage === "/index.html") {
+    displaySlider();
     displayPopularMovies();
   } else if (global.currentPage === "/shows.html") {
     displayPopularShows();
